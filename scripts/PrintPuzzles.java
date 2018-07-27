c107cdd76bdff74ddd27cd8621b496f9f038449e
Justin Venezuela <jven@mit.edu>
2018-07-26 23:52:05 -0400

    Sets some code style settings.

--- scripts/PrintPuzzles.java
+++ scripts/PrintPuzzles.java
@@ -3,7 +3,6 @@ package scripts;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
